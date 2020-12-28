30bd657e1262adecfa2463c5e2e824254ce86a19
Vasu Gupta <guptavasu1213@gmail.com>
2020-12-28 01:08:28 -0700

    Implement learn more section for Projects tab

--- js/projects.js
+++ js/projects.js
@@ -5,12 +5,33 @@ function fillProjectsInfo(projectData) {
     const result = renderFn(projectData);
     document.querySelector("#projects").innerHTML = result;
 }
+function fillLearnMorePopup(singleProjectData) {
+    const template = document.querySelector("#learn-more-template").innerHTML;
+    const renderFn = doT.template(template);
+    const result = renderFn(singleProjectData);
+    let popup = document.querySelector("#learn-more-popup");
+    popup.innerHTML = result;
+    let closeBtn = document.querySelector("#learn-more-popup .close");
+    closeBtn.addEventListener("click", function () {
+        popup.style.visibility = "hidden";
+    });
+}
+function attachLearnMoreListeners(json) {
+    let learnBtns = document.querySelectorAll(".learn-more-btn");
+    for (let i = 0; i < learnBtns.length; i++) {
+        learnBtns[i].addEventListener("click", function (event) {
+            fillLearnMorePopup(json[i]);
+            document.querySelector("#learn-more-popup").style.visibility = "visible";
+        });
+    }
+}
 function retrieveProjectsAndFill() {
     fetch("./data/projects.json")
         .then(response => response.json())
         .then(json => {
         console.log(json);
         fillProjectsInfo(json);
+        attachLearnMoreListeners(json);
     });
 }
 retrieveProjectsAndFill();
